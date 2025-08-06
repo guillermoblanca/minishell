72f190af095f9b0375955eb460b327da3014904e
Your Name <you@example.com>
2025-08-06 20:47:15 +0200

    exit

--- ft_exit.c
+++ ft_exit.c
@@ -5,17 +5,25 @@ void    ft_exit(char **command, t_shell *shell)
         shell->exit = 1;
         write(shell->outfd, "exit\n", 5);
         if (command[0] && command[1])
-                write_exit_toomany(shell);
+                print_exit_error(shell);
         else if (command[0] && ft_isdigit_str(command[0]) == 0)
-                write_numeric_err(shell, command);
+                print_num_error(shell, command);
         else if (command[0])
                 shell->g_status = ft_atoi(command[0]);
         else
                 shell->g_status = 0;
 }
 
-void	write_exit(t_shell *shell)
+void	print_exit_error(t_shell *shell)
 {
-	write(STDERR_FILENO, "exit: too many arguments\n", 26);
+	write(STDERR_FILENO, "exit: many arguments\n", 22);
+	shell->g_status = 1;
+}
+
+void	print_num_error(t_shell *shell, char **command)
+{
+	write(STDERR_FILENO, "exit", 4);
+	write(STDERR_FILENO, command[0], ft_strlen(command[0]));
+	write(STDERR_FILENO, "num arg requiered", 17);
 	shell->g_status = 1;
 }
